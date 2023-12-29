90208e73ba4e7f78191838edb8984b093ca93f1e
Yessi3 <yasminekoulin@gmail.com>
2023-12-29 10:31:43 +0100

    Update movies.sql

--- server/movies.sql
+++ server/movies.sql
@@ -1,2 +1,6 @@
-CREATE TABLE IF EXISTS movies;
-CREATE TABLE IF NOT EXISTS movies;
+CREATE TABLE movies (
+   movie_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+   title VARCHAR(120),
+   year_released INTEGER,
+   director VARCHAR(80)
+);
