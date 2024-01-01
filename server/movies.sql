20ee68227c4d7c85f4852f11f0d62dffdd977fc1
intesar-create <h20intma@du.se>
2024-01-01 17:13:29 +0100

    Update movies.sql

--- server/movies.sql
+++ server/movies.sql
@@ -5,9 +5,9 @@ CREATE TABLE IF NOT EXISTS movies (
    year_released INTEGER NOT NULL,
    director VARCHAR(80) NOT NULL,
 );
-INSERT INTO movies(id,title, year_released,director) VALUES (1,'','','');
-INSERT INTO movies(id,title, year_released,director) VALUES (2,'','','');
-INSERT INTO movies(id,title, year_released,director) VALUES (3,'','','');
+INSERT INTO movies(id,title, year_released,director) VALUES (1,'Parasite',2019,'Bong Joon Ho');
+INSERT INTO movies(id,title, year_released,director) VALUES (2,'Time to Hunt',2020,'Yoon Sunghyun');
+INSERT INTO movies(id,title, year_released,director) VALUES (3,'The Pirates: The Last Royal Treasure',2022,'Jeong-hoon Kim');
 
 
 select * from movies;
