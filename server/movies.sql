3619942651d156b6294f649ecaf74af59a70310f
intesar-create <h20intma@du.se>
2024-01-02 12:11:37 +0100

    Update movies.sql

--- server/movies.sql
+++ server/movies.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS movies(
    id        INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT
   ,titel VARCHAR(8) NOT NULL
   ,dirctor  VARCHAR(9) NOT NULL
-  ,year         INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT
+  ,release_date         INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT
   ,color     VARCHAR(6) NOT NULL
 );
-INSERT INTO movies(id,titel,dirctor,year,color) VALUES (2,'Muhammad','Torphy',2019,'gray');
-INSERT INTO movies(id,titel,dirctor,year,color) VALUES (3,'Carlee','Tromp',2020,'purple');
-INSERT INTO movies(id,titel,dirctor,year,color) VALUES (1,'Zena','Zulauf',2022,'green');
+INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (2,'Muhammad','Torphy',2019,'gray');
+INSERT INTO movies(id,titel,dirctorrelease_date,color) VALUES (3,'Carlee','Tromp',2020,'purple');
+INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (1,'Zena','Zulauf',2022,'green');
 
 
 select * from movies;
