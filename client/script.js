dc6695567f88df84fee7bf509338de4a126c198d
safmo <120182390+safmo@users.noreply.github.com>
2024-01-03 11:07:32 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,32 +1,4 @@
-//const url = 'http://localhost:3000/movies';
-//
-//window.addEventListener('load', fetchData);
-
-//function fetchData() {
-  //fetch(url)
-   // .then((result) => result.json())
-    //.then((movies) => {
-      //if (movies.length > 0) {
-
-
-      //  const listContainer = document.getElementById('resourceList');
-       // listContainer.innerHTML = '';
-        //listContainer.insertAdjacentHTML('beforeend', html);
-      //}
-   // });
-//}
-
-//const resourceForm = document.getElementById('resourceForm');
-//console.log(resourceForm);
-//resourceForm.addEventListener("submit", handleSubmit);
-
-//function handleSubmit(e) {
- // e.preventDefault(); // Förhindra standardbeteendet för formuläret
- // console.log(e); // Se till att event-objektet används korrekt
-//}
-
-
-    // Assuming you have an API endpoint to fetch movies data
+ // Assuming you have an API endpoint to fetch movies data
     const moviesEndpoint = 'http://localhost:3000/movies';
 
     // Function to fetch movies from the server
