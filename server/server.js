3ffcbfabddfcf0129cd3128afb4cdef6246c4b74
intesar-create <h20intma@du.se>
2024-01-05 13:11:20 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const sqlite = require('sqlite3').verbose();
-const db = new sqlite.Database('');
+const db = new sqlite.Database('./gik339-projekt.db');
 
 const express = require('express');
 const server = express();
