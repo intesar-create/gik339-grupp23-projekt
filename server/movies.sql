d4ad7ea360907d572d9dfab3813f9e34fe2a1e9a
safmo <120182390+safmo@users.noreply.github.com>
2024-01-05 11:12:16 +0100

    Update movies.sql

--- server/movies.sql
+++ server/movies.sql
@@ -6,9 +6,12 @@ CREATE TABLE IF NOT EXISTS movies(
   ,release_date   INTEGER  NOT NULL
   ,color          VARCHAR(6) NOT NULL
 );
-INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (2,'Parasite','Bong Joon Ho',2019,'gray');
-INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (3,'Time to Hunt','Yoon Sunghyun',2020,'purple');
-INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (1,'The Pirates: The Last Royal Treasure','Jeong-hoon Kim',2022,'green');
+INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (1,'Parasite','Bong Joon Ho',2019,'gray');
+INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (2,'Time to Hunt','Yoon Sunghyun',2020,'purple');
+INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (3,'The Pirates: The Last Royal Treasure','Jeong-hoon Kim',2022,'green');
+INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (4,'Forrest Gump','Robert Zemeckis',1994,'green');
+INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (5,'Finding Nemo','Andrew Stanton ,Lee Unkrich',2003,'purple');
+INSERT INTO movies(id,titel,dirctor,release_date,color) VALUES (6,'Inside out','Pete Docter, Ronnie Del Carmen',2015,'green');
 
 
 select * from movies;
